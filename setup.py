5b5873d5ac8c270b23340aca070d06a547e058c3
Vladimir Goncharov <vladimarius@gmail.com>
2015-05-20 15:29:09 +0300

    Version update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='imapy',
-      version='1.0.2',
+      version='1.0.3',
       description='Imap for humans',
       long_description=readme(),
       keywords='imap library, email processing, imaplib',
